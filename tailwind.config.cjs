ba44eee9bb53fd9deab79efe262ebb02194071e9
Falodun Damilola <dpaulzs11@gmail.com>
2023-02-07 23:36:49 +0100

    images and tailwind configuration

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,22 @@
 module.exports = {
   content: [],
   theme: {
-    extend: {},
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px'
+    },
+    extend: {
+      colors: {
+        lightgray: "#a2a2a2",
+        dark:"#404040",
+        highlight:"#48b5e9",
+      },
+      fontFamily: {
+        archivo:['Archivo', 'sans-serif']
+      }
+    },
   },
   plugins: [],
 }
